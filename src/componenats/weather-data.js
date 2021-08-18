b45e26fcc5ea0ad990a740bb622a5cfeaca11785
Ritesh Jangir <79959578+mrriteshjangir@users.noreply.github.com>
2021-08-18 15:17:42 +0530

    Update weather-data.js

--- src/componenats/weather-data.js
+++ src/componenats/weather-data.js
@@ -2,32 +2,70 @@ import React,{useState,useEffect} from 'react';
 
 function Weatherdata(){
     
-    const key ='e624663fa43f10108456852a73658e56';
-    const [feels_like,setFeelsLike]=useState('');
-    const [mainTemp,setMainTemp1]=useState('');
-    const [description,setDescription]=useState('');
-    const [main1,setMain]=useState('');
+    const key="3db3178688864d9d83540815212607";
+
+    const[all_data,setAllData]=useState('');
+
+    const[city_name,setCityName]=useState('nagpur');
 
     useEffect(()=>{
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=nagpur&appid='+key)
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            setFeelsLike(data.main.feels_like);
-            setMainTemp1(data.main.temp);
-            setDescription(data.weather[0].description);
-            setMain(data.weather[0].main);
-        })
-    },[])
-    return(
-        <>
+        const getTemp=async()=>{
+          fetch('http://api.weatherapi.com/v1/current.json?key='+key+'>&q='+city_name+'')
+          .then(res=>res.json())
+          .then(abc=>{
+            setAllData(abc.current);
+
+          })
+        };
+
+        getTemp();
+
+    },[city_name] )
+
+    var a=10;
+
+  return (
+    <div className="container mt-3">
+        <div className="row mb-3">
+            <div className="col-md-12">
+            <input type="text" class="form-control" value={city_name} onChange={(e)=>{setCityName(e.target.value)}} placeholder="Enter City Name"/>
+            </div>
+
+        </div>
+      <table class="table table-dark table-hover">
+
+        
+        <thead>
         <tr>
-            <td>{mainTemp}</td>
-            <td>{feels_like}</td>
-            <td>{main1}</td>
-            <td>{description}</td>
-        </tr> 
-        </>
-    )
+            <th scope="col"> Max Temp</th>
+            <th scope="col">Min-Temp</th>
+            <th scope="col">Pressure</th>
+            <th scope="col">Humidity</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <tr>
+             
+                <td>
+                  
+                {typeof all_data=="undefined" ? "Loding" : all_data.temp_c} °
+                </td>
+                {/* <td>
+                {typeof all_data=="undefined" ? "Loding" : all_data.temp_min} °
+                </td>
+                <td>
+                {typeof all_data=="undefined" ? "Loding" : all_data.pressure}
+                </td>
+                <td>
+                {typeof all_data=="undefined" ? "Loding" : all_data.humidity}
+                </td> */}
+                
+            
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  );
     }
 export default Weatherdata;
